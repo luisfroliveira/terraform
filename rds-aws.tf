74d6df6d8a39fea607ee4a1eb98425c1bf9b21fe
luisfroliveira <infotec.luis.oliveira@gmail.com>
2023-01-04 18:27:20 -0300

    Add files via upload
    RDS com MySQL

---
+++ rds-aws.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region  = "sa-east-1"
+  access_key = "chave de acesso"
+  secret_key = "chave secreta"
+}
+
+resource "aws_db_instance" "mysql-terraform" {
+  allocated_storage    = 20
+  max_allocated_storage = 100
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "luis"
+  password             = "luis1234"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  availability_zone = "sa-east-1c"
+
+  tags = {
+    Name        = "Meu MySQL"
+    Environment = "DevOps"
+  }
+
+}
